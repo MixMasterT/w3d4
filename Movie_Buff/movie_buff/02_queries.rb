51f7c9d3cd56ce506d95b5947a6340fce6861520
App Academy Student <student@appacademy.io>
2016-12-01 18:17:35 -0800

    Complete 02 Queries, and work on 03

--- Movie_Buff/movie_buff/02_queries.rb
+++ Movie_Buff/movie_buff/02_queries.rb
@@ -22,9 +22,11 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  Actor.select(:id, :name)
-        .joins(:movies)
+  Movie.select('actors.id, actors.name')
+        .joins(:actors)
         .where("movies.title = ?", title)
+        .group('actors.id','castings.ord')
+        .order('castings.ord')
 
 
 end
@@ -34,11 +36,18 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie.select("movies.id, movies.title, actors.name")
+        .joins(:actors)
+        .where("movies.director_id = actors.id AND castings.ord = 1")
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor.select("actors.id, actors.name, COUNT(castings.*) AS roles")
+        .joins(:castings)
+        .where("castings.ord > 1")
+        .group('actors.id')
+        .order("COUNT(castings.*) DESC")
+        .limit(2)
 end
